1c8cac037816e92cf5a14ed0fb5d90dfb8e84d87
matt zhou <matt@cruelyouth.com>
2020-11-19 19:12:26 +0800

    calendar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import Calendar from './components/calendar'
+import React from 'react'
+class App extends React.Component {
+  state = {
+    date: '2020-11-08',
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <Calendar />
-    </div>
-  )
+  render() {
+    return (
+      <div className="App">
+        <Calendar
+          value={this.state.date}
+          onChange={(newDate) =>
+            this.setState({
+              date: newDate,
+            })
+          }
+        />
+      </div>
+    )
+  }
 }
 
 export default App
--- src/components/calendar/index.jsx
+++ src/components/calendar/index.jsx
@@ -1,106 +1,129 @@
 import React from 'react'
-import styles from './style.css'
-console.log(styles)
+import PropTypes from 'prop-types'
+import moment from 'moment'
+import cx from 'classnames'
+import 'moment/locale/zh-cn'
+import './style.css'
+
+window.moment = moment
+
+moment.locale('zh-cn')
+
+function parseDateFromString(value) {
+  const m = moment(value)
+  return {
+    year: m.year(),
+    month: m.month(),
+    date: m.date(),
+  }
+}
 
 class Calendar extends React.Component {
+  static propTypes = {
+    value: PropTypes.string,
+    onChange: PropTypes.func,
+  }
+
+  constructor(props) {
+    super(props)
+    const d = parseDateFromString(this.props.value || Date.now())
+    this.state = {
+      currentYear: d.year,
+      currentMonth: d.month,
+      // currentDate: d.date,
+    }
+  }
+
+  componentDidMount() {}
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    const { value } = this.props
+    const { value: prevValue } = prevProps
+    if (value !== prevValue) {
+      const date = moment(value)
+      if (this.state.currentMonth !== date.month()) {
+        this.setState({
+          currentYear: date.year(),
+          currentMonth: date.month(),
+        })
+      }
+    }
+  }
+
+  handlePreviousMonth = () => {
+    this.monthChange(-1)
+  }
+
+  handleNextMonth = () => {
+    this.monthChange(1)
+  }
+
+  monthChange = (change) => {
+    const { currentYear, currentMonth } = this.state
+    const current = moment([currentYear, currentMonth])
+    const next = current.clone().add(change, 'M')
+    this.setState({
+      currentYear: next.year(),
+      currentMonth: next.month(),
+    })
+  }
+
+  handleDateChoose = (start, row, column) => {
+    const dateDiff = row * 7 + column
+    const date = start.clone().add(dateDiff, 'days')
+    this.props.onChange(date.format('YYYY-MM-DD'))
+  }
+
   render() {
+    const { currentYear, currentMonth } = this.state
+    const monthStart = moment([currentYear, currentMonth])
+    const calendarStart = monthStart.clone().subtract(monthStart.weekday(), 'd')
+    const monthEnd = monthStart.clone().date(monthStart.daysInMonth())
+    const today = moment()
+    const active = this.props.value ? moment(this.props.value) : null
+    const daysDiff = moment.duration(monthEnd.diff(calendarStart)).asDays() + 1
+
     return (
       <div className="container">
         <div className="calendar">
           <div className="front">
             <div className="current-date">
-              <div className="left"></div>
-              <div className="right"></div>
-              <h1>January 2016</h1>
+              <div className="left" onClick={this.handlePreviousMonth}></div>
+              <div className="right" onClick={this.handleNextMonth}></div>
+              <h1>{monthStart.format('YYYY年MM月')}</h1>
             </div>
             <div className="current-month">
               <ul className="week-days">
-                <li>MON</li>
-                <li>TUE</li>
-                <li>WED</li>
-                <li>THU</li>
-                <li>FRI</li>
-                <li>SAT</li>
-                <li>SUN</li>
+                {moment.weekdaysShort(true).map((n) => (
+                  <li key={n}>{n}</li>
+                ))}
               </ul>
               <div className="weeks">
-                <div className="first">
-                  <span className="last-month">28</span>
-                  <span className="last-month">29</span>
-                  <span className="last-month">30</span>
-                  <span className="last-month">31</span>
-                  <span>01</span>
-                  <span>02</span>
-                  <span>03</span>
-                </div>
-                <div className="second">
-                  <span>04</span>
-                  <span>05</span>
-                  <span className="event">06</span>
-                  <span>07</span>
-                  <span>08</span>
-                  <span>09</span>
-                  <span>10</span>
-                </div>
-                <div className="third">
-                  <span>11</span>
-                  <span>12</span>
-                  <span>13</span>
-                  <span>14</span>
-                  <span className="active">15</span>
-                  <span>16</span>
-                  <span>17</span>
-                </div>
-                <div className="fourth">
-                  <span>18</span>
-                  <span>19</span>
-                  <span>20</span>
-                  <span>21</span>
-                  <span>22</span>
-                  <span>23</span>
-                  <span>24</span>
-                </div>
-                <div className="fifth">
-                  <span>25</span>
-                  <span>26</span>
-                  <span>27</span>
-                  <span>28</span>
-                  <span>29</span>
-                  <span>30</span>
-                  <span>31</span>
-                </div>
+                {[...Array(Math.ceil(daysDiff / 7))].map((_, row) => (
+                  <div key={row}>
+                    {[...Array(7)].map((_, column) => {
+                      const d = calendarStart
+                        .clone()
+                        .add(row * 7 + column, 'day')
+                      return (
+                        <span
+                          key={column}
+                          className={cx({
+                            'last-month': d.month() !== monthStart.month(),
+                            today: d.isSame(today, 'date'),
+                            active: active && d.isSame(active, 'date'),
+                          })}
+                          onClick={() =>
+                            this.handleDateChoose(calendarStart, row, column)
+                          }
+                        >
+                          {d.format('DD')}
+                        </span>
+                      )
+                    })}
+                  </div>
+                ))}
               </div>
-            </div>
-          </div>
-          <div className="back">
-            <input placeholder="What's the event?" />
-            <div className="info">
-              <div className="date">
-                <p className="info-date">
-                  Date: <span>Jan 15th, 2016</span>
-                </p>
-                <p className="info-time">
-                  Time: <span>6:35 PM</span>
-                </p>
-              </div>
-              <div className="address">
-                <p>
-                  Address: <span>129 W 81st St, New York, NY</span>
-                </p>
-              </div>
-              <div className="observations">
-                <p>
-                  Observations: <span>Be there 15 minutes earlier</span>
-                </p>
-              </div>
-            </div>
-            <div className="actions">
-              <button className="save">
-                Save <i className="ion-checkmark" />
-              </button>
-              <button className="dismiss">
-                Dismiss <i className="ion-android-close" />
-              </button>
             </div>
           </div>
         </div>
